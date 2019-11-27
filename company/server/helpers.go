22748e309846a42866c6eb27b0f052857a881ee3
cuijxin <cuijxin@126.com>
2019-11-27 08:50:31 +0800

    test

--- company/server/helpers.go
+++ company/server/helpers.go
@@ -1,6 +1,12 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"regexp"
+	"strings"
+	"time"
+
 	"google.golang.org/grpc/metadata"
 
 	"v2.staffjoy.com/auditlog"
@@ -39,3 +45,48 @@ func newAuditEntry(md metadata.MD, targetType, targetUUID, companyUUID, teamUUID
 // 	}
 // 	return grpc.Errorf(codes.Unknown, format, a...)
 // }
+
+func sanitizeDayOfWeek(input string) (string, error) {
+	input = strings.TrimSpace(strings.ToLower(input))
+	for _, day := range daysOfWeek {
+		if day == input {
+			return input, nil
+		}
+	}
+	return "", fmt.Errorf("unknown day of week")
+}
+
+func validTimezone(input string) (err error) {
+	_, err = time.LoadLocation(input)
+	return
+}
+
+func validColor(input string) error {
+	re := regexp.MustCompile(`^[0-9A-Fa-f]{3}$|^[0-9A-Fa-f]{6}$`)
+	if !re.MatchString(input) {
+		return fmt.Errorf("invalid color hex code")
+	}
+	return nil
+}
+
+func getAuth(ctx context.Context) (md metadata.MD, authz string, err error) {
+	md, ok := metadata.FromIncomingContext(ctx)
+	if !ok {
+		return nil, "", fmt.Errorf("Context missing metadata")
+	}
+	if len(md[auth.AuthorizationMetadata]) == 0 {
+		return nil, "", fmt.Errorf("Missing Authorization")
+	}
+	authz = md[auth.AuthorizationMetadata][0]
+	return
+}
+
+// func copyAccountToDirectory(a *account.Account, d *pb.DirectoryEntry) {
+// 	d.UserUuid = a.Uuid
+//
+// }
+
+func asyncContext() context.Context {
+	md := metadata.New(map[string]string{auth.AuthorizationMetadata: auth.AuthorizationCompanyService})
+	return metadata.NewOutgoingContext(context.Background(), md)
+}
--- go.mod
+++ go.mod
@@ -7,21 +7,22 @@ require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/fatih/structs v1.1.0
 	github.com/getsentry/raven-go v0.2.0
+	github.com/go-gorp/gorp v2.2.0+incompatible
 	github.com/gogo/protobuf v1.3.1
 	github.com/golang/protobuf v1.3.2
 	github.com/google/go-querystring v1.0.0 // indirect
-	github.com/grpc-ecosystem/grpc-gateway v1.11.3
+	github.com/grpc-ecosystem/grpc-gateway v1.9.0
 	github.com/keighl/mandrill v0.0.0-20170605120353-1775dd4b3b41
 	github.com/pkg/errors v0.8.1 // indirect
 	github.com/sirupsen/logrus v1.4.2
-	github.com/spf13/cobra v0.0.5
+	github.com/spf13/cobra v0.0.5 // indirect
 	github.com/stretchr/testify v1.2.2
 	github.com/ttacon/builder v0.0.0-20170518171403-c099f663e1c2 // indirect
 	github.com/ttacon/libphonenumber v1.0.1
-	golang.org/x/crypto v0.0.0-20181203042331-505ab145d0a9
-	golang.org/x/net v0.0.0-20190213061140-3a22650c66bd
+	golang.org/x/crypto v0.0.0-20190308221718-c2843e01d9a2
+	golang.org/x/net v0.0.0-20191002035440-2ec189313ef0
 	google.golang.org/genproto v0.0.0-20191009194640-548a555dbc03 // indirect
-	google.golang.org/grpc v1.19.0
+	google.golang.org/grpc v1.24.0
 	gopkg.in/intercom/intercom-go.v2 v2.0.0-20190726192626-b9d655ee17b8
 	gopkg.in/yaml.v2 v2.2.4 // indirect
 )
